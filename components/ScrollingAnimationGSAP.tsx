f221b417bfc43a734a5aeb07edffa0ab11eab68b
IbraheemHaseeb7 <ibraheemhaseeb7@gmail.com>
2024-01-05 22:40:26 +0500

    removed markers

--- components/ScrollingAnimationGSAP.tsx
+++ components/ScrollingAnimationGSAP.tsx
@@ -6,7 +6,7 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 export default function ScrollingAnimationGSAP() {
   const divRef: any = useRef();
-  useEffect(() => {
+  useGSAP(() => {
     const el = divRef.current;
     const timeline = gsap.timeline({
       scrollTrigger: {
@@ -14,12 +14,11 @@ export default function ScrollingAnimationGSAP() {
         scrub: true,
         start: "top bottom",
         end: "300% bottom",
-        markers: true,
       },
     });
     timeline.from(el, { scale: 1 });
     timeline.to(el, { scale: 5 });
-  }, []);
+  }, {});
 
   return (
     <div className="w-full h-[50rem] flex justify-center items-center">
